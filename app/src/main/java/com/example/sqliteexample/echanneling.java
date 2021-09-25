86e2ca13d6741ef52ae3d454ddfcf6559dbfee67
ITharu123 <89218316+ITharu123@users.noreply.github.com>
2021-09-25 16:03:35 +0530

    IT20246228 - 8

---
+++ app/src/main/java/com/example/sqliteexample/echanneling.java
@@ -0,0 +1,21 @@
+package com.example.doctormanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class echanneling extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_echanneling);
+    }
+    public void openMain11(View view){ startActivity(new Intent(this,DoctorManagement.class  )); }
+
+}
+
+
+
