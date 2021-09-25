a814d5d39a9b8ca072a5d1bdb7806b05522bbd56
KawinduLukshan <89626366+KawinduLukshan@users.noreply.github.com>
2021-09-25 22:23:46 +0530

    Update ViewPharmacyActivity.java

--- app/src/main/java/com/example/sqliteexample/ViewPharmacyActivity.java
+++ app/src/main/java/com/example/sqliteexample/ViewPharmacyActivity.java
@@ -3,15 +3,14 @@ package com.example.sqliteexample;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
 import android.os.Bundle;
 import android.widget.Toast;
-
 import java.util.List;
 
 public class ViewPharmacyActivity extends AppCompatActivity {
 
     RecyclerView recyclerView;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,16 +22,17 @@ public class ViewPharmacyActivity extends AppCompatActivity {
 
         database1 database12 = new database1(this);
         List<PharmacyModelClass> pharmacyModelClasses = database12.getPharmacyList();
+        
 
         if (pharmacyModelClasses.size() > 0){
             PharmacyAdapterClass pharmacyadapterclass = new PharmacyAdapterClass(pharmacyModelClasses,ViewPharmacyActivity.this);
             recyclerView.setAdapter(pharmacyadapterclass);
-        }else {
+            
+        }else
+            
+           //	When there is no prescription to look at 
+       {
             Toast.makeText(this, "There is no Pharmacy in the database", Toast.LENGTH_SHORT).show();
         }
-
-
-
-
     }
 }
