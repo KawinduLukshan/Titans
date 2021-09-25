18dae52203119760e9e816fb421a714111e71bc1
ITharu123 <89218316+ITharu123@users.noreply.github.com>
2021-09-25 16:10:38 +0530

    IT20246228 - 9

---
+++ app/src/main/java/com/example/sqliteexample/LevelsOfDoctors.java
@@ -0,0 +1,19 @@
+package com.example.doctormanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class LevelsOfDoctors extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_levels_of_doctors);
+    }
+    public void openMain13(View view){ startActivity(new Intent(this,Doctor_Home_Page.class  )); }
+
+}
+
