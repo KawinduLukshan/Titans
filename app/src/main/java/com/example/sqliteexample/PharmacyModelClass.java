2992094c2f625513a20eb422c3cce20fc6332659
KawinduLukshan <89626366+KawinduLukshan@users.noreply.github.com>
2021-09-25 22:26:43 +0530

    Update PharmacyModelClass.java

--- app/src/main/java/com/example/sqliteexample/PharmacyModelClass.java
+++ app/src/main/java/com/example/sqliteexample/PharmacyModelClass.java
@@ -1,5 +1,6 @@
 package com.example.sqliteexample;
 
+
 public class PharmacyModelClass {
 
     Integer id;
@@ -26,54 +27,68 @@ public class PharmacyModelClass {
         this.p_PharmacyName = p_PharmacyName;
         this.p_PrescriptionName = p_PrescriptionName;
     }
-
+//get
     public Integer getId() {
         return id;
     }
-
+    
+//set
     public void setId(Integer id) {
         this.id = id;
     }
-
+    
+    
+//get
     public String getpName() {
         return p_Name;
     }
-
+    
+//set
     public void setpName(String pName) {
         this.p_Name = p_Name;
     }
-
+    
+    
+//get
     public String getpAddress() {
         return p_Address;
     }
-
+    
+//set
     public void setpAddress(String pAddress) {
         this.p_Address = p_Address;
     }
+    
+    
+ //get
     public String getpMobile() {
         return p_Mobile;
     }
-
+    
+//set
     public void setpMobile(String pMobile) {
         this.p_Mobile = p_Mobile;
     }
-
+    
+    
+//get
     public String getpPharmacyName() {
         return p_PharmacyName;
     }
-
+    
+//set
     public void setpPharmacyName(String pPharmacyName) {
         this.p_PharmacyName = p_PharmacyName;
     }
-
+    
+    
+//get
     public String getpPrescriptionName() {
         return p_PrescriptionName;
     }
-
+    
+//set
     public void setpPrescriptionName(String pPrescriptionName) {
         this.p_PrescriptionName = p_PrescriptionName;
     }
-
-
-
 }
