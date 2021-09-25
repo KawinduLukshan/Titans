fb9476eaad087b12fa33941cf8ee1702de653532
krishmipaboda <89625459+krishmipaboda@users.noreply.github.com>
2021-09-26 00:08:43 +0530

    IT20158422 (4)

---
+++ app/src/main/java/com/example/sqliteexample/Home_page.java
@@ -0,0 +1,30 @@
+package com.example.e_channelling;
+
+        import androidx.appcompat.app.AppCompatActivity;
+        import android.content.Intent;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.Button;
+
+public class Home_page extends AppCompatActivity {
+    public Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_page);
+
+        button = (Button) findViewById(R.id.button5);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(Home_page.this,Patient_SIGN_IN.class);
+                startActivity(intent);
+            }
+
+        });
+    }
+
+}
+
