585605527650eb99d24964b00b191d661c550111
KawinduLukshan <89626366+KawinduLukshan@users.noreply.github.com>
2021-09-26 09:45:51 +0530

    Update database1.java

--- app/src/main/java/com/example/sqliteexample/database1.java
+++ app/src/main/java/com/example/sqliteexample/database1.java
@@ -10,9 +10,7 @@ import java.util.List;
 
 public class database1 extends SQLiteOpenHelper {
 
-    
-//SQLite
-//database
+
     
    //Database version
     private static final int DATABASE_VERSION = 5;
