9b49f19081a7d1958528f46379c8f2567accc2dd
ITharu123 <89218316+ITharu123@users.noreply.github.com>
2021-09-26 10:02:46 +0530

    IT20246228 new

--- app/src/main/java/com/example/sqliteexample/DoctorSchedule.java
+++ app/src/main/java/com/example/sqliteexample/DoctorSchedule.java
@@ -13,6 +13,7 @@ public class DoctorSchedule extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doctor_schedule);
     }
+    //button connect
     public void openMain30(View view){ startActivity(new Intent(this,Doctor_Home_Page.class  )); }
 
 }
