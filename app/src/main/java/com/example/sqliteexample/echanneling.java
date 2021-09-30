0814950ecbd0fc7280cf12ca9169df269b28deff
IT20001834 <89619736+IT20001834@users.noreply.github.com>
2021-09-30 11:46:26 +0530

    Update echanneling.java

--- app/src/main/java/com/example/sqliteexample/echanneling.java
+++ app/src/main/java/com/example/sqliteexample/echanneling.java
@@ -7,12 +7,32 @@ import android.os.Bundle;
 import android.view.View;
 
 public class echanneling extends AppCompatActivity {
+    
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_echanneling);
     }
+    //Channel Doctor button
+    //public void openpart1(View view){
+      //  Intent intent_openpart1 = new Intent(this,MainActivity.class);
+        //startActivity(intent_openpart1);
+    }
+    //User Management button
+    //public void openpart2(View view){
+      //  Intent intent_openpart1 = new Intent(this,MainActivity.class);
+       // startActivity(intent_openpart1);
+    //}
+    
+    //Pharmacy/Lab button
+    //public void openpart1(View view){
+      //  Intent intent_openpart3 = new Intent(this,MainActivity.class);
+        //startActivity(intent_openpart1);
+    }
+    
+    //Doctor Management button
     public void openMain11(View view){ startActivity(new Intent(this,DoctorManagement.class  )); }
 
 }
