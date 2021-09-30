eb9239f00583ce7bf56a75ff3f4a2e046e2d0a31
IT20001834 <89619736+IT20001834@users.noreply.github.com>
2021-09-30 07:45:57 +0530

    new commit it20001834

--- app/src/main/java/com/example/sqliteexample/PA_Master.java
+++ app/src/main/java/com/example/sqliteexample/PA_Master.java
@@ -13,10 +13,13 @@ public final class PA_Master {
         public static final String COLUMN_NAME_PNIC1 = "p_nic1";
         public static final String COLUMN_NAME_PPHONE1 = "p_phone1";
         public static final String COLUMN_NAME_PDATE1 = "p_date1";
+        public static final String COLUMN_NAME_DOC1 = "p_doctor1";
+        public static final String COLUMN_NAME_HOS1 = "p_hospital1";
     }
 
     public static class Appointment_Pay implements BaseColumns{
         public static final String TABLE_NAME_02 = "appointment_pay";
+        public static final String COLUMN_NAME_BANKNAME = "p_bankname1";
         public static final String COLUNM_NAME_CARDNO = "pcardno";
         public static final String COLUNM_NAME_CVV = "cvvno";
         public static final String COLUMN_NAME_EXPIREDATE = "expiredate";
