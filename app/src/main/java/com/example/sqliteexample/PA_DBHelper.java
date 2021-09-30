20de949584972deb6225a35dee36327cef240d85
IT20001834 <89619736+IT20001834@users.noreply.github.com>
2021-09-30 07:44:44 +0530

    new commit it20001834

--- app/src/main/java/com/example/sqliteexample/PA_DBHelper.java
+++ app/src/main/java/com/example/sqliteexample/PA_DBHelper.java
@@ -23,12 +23,15 @@ public class PA_DBHelper extends SQLiteOpenHelper {
                PA_Master.Pappointment.COLUMN_NAME_PAGE1+" TEXT,"+
                PA_Master.Pappointment.COLUMN_NAME_PNIC1+" TEXT,"+
                PA_Master.Pappointment.COLUMN_NAME_PPHONE1+" TEXT,"+
-               PA_Master.Pappointment.COLUMN_NAME_PDATE1+" TEXT)";
+               PA_Master.Pappointment.COLUMN_NAME_PDATE1+" TEXT,"+
+               PA_Master.Pappointment.COLUMN_NAME_DOC1+" TEXT,"+
+               PA_Master.Pappointment.COLUMN_NAME_HOS1+" TEXT)";
 
         adb.execSQL(SQL_P_APPOINTMENT_CREATE);
 
         String SQL_APPOINTMANTPAY_CREATE = "CREATE TABLE "+PA_Master.Appointment_Pay.TABLE_NAME_02+" ("+
                 PA_Master.Appointment_Pay._ID+" INTEGER PRIMARY KEY,"+
+                PA_Master.Appointment_Pay.COLUMN_NAME_BANKNAME+" TEXT,"+
                 PA_Master.Appointment_Pay.COLUNM_NAME_CARDNO+" TEXT,"+
                 PA_Master.Appointment_Pay.COLUNM_NAME_CVV+" TEXT,"+
                 PA_Master.Appointment_Pay.COLUMN_NAME_EXPIREDATE+" TEXT)";
@@ -39,7 +42,7 @@ public class PA_DBHelper extends SQLiteOpenHelper {
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) { }
 
-    public long addappointment(String p_name1,String p_age1,String p_nic1,String p_phone1,String p_date1){
+    public long addappointment(String p_name1,String p_age1,String p_nic1,String p_phone1,String p_date1,String p_doctor1, String p_hospital1){
         SQLiteDatabase adb = getWritableDatabase();
 
         ContentValues cval = new ContentValues();
@@ -49,6 +52,8 @@ public class PA_DBHelper extends SQLiteOpenHelper {
         cval.put(PA_Master.Pappointment.COLUMN_NAME_PNIC1,p_nic1);
         cval.put(PA_Master.Pappointment.COLUMN_NAME_PPHONE1,p_phone1);
         cval.put(PA_Master.Pappointment.COLUMN_NAME_PDATE1,p_date1);
+        cval.put(PA_Master.Pappointment.COLUMN_NAME_DOC1,p_doctor1);
+        cval.put(PA_Master.Pappointment.COLUMN_NAME_HOS1,p_hospital1);
 
         long RowId1 = adb.insert(PA_Master.Pappointment.TABLE_NAME_1,null,cval);
         return RowId1;
@@ -84,11 +89,12 @@ public class PA_DBHelper extends SQLiteOpenHelper {
     }
 
     //add new payment
-    public long appointmentpayment(String pcardno ,String cvvno ,String expiredate){
+    public long appointmentpayment(String p_bankname1,String pcardno ,String cvvno ,String expiredate){
         SQLiteDatabase pdb = getWritableDatabase();
 
         ContentValues cval2 =new ContentValues();
 
+        cval2.put(PA_Master.Appointment_Pay.COLUMN_NAME_BANKNAME,p_bankname1);
         cval2.put(PA_Master.Appointment_Pay.COLUNM_NAME_CARDNO,pcardno);
         cval2.put(PA_Master.Appointment_Pay.COLUNM_NAME_CVV,cvvno);
         cval2.put(PA_Master.Appointment_Pay.COLUMN_NAME_EXPIREDATE,expiredate);
@@ -97,3 +103,4 @@ public class PA_DBHelper extends SQLiteOpenHelper {
         return RowID2;
     }
 }
+
